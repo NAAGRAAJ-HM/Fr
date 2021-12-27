0adc390cd3144e207201aed8b9d5bd6d7c3ed219
root <nagaraja.hm.0001@gmail.com>
2021-12-27 22:19:54 +0100

    Update: submodule Fr

---
+++ LATEST/Fr.cpp
@@ -0,0 +1,7 @@
+#include "Fr.h"
+
+class_Fr Fr;
+
+void class_Fr::InitFunction(void){
+}
+
---
+++ LATEST/Fr.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Fr{
+   public:
+      void InitFunction(void);
+};
+
+extern class_Fr Fr;
+
