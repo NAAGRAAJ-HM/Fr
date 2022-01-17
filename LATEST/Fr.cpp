b233fed21adf8669601563194ddba992333a9d95
root <nagaraja.hm.0001@gmail.com>
2022-01-17 10:27:06 +0100

    Update: submodule Fr

--- LATEST/Fr.cpp
+++ LATEST/Fr.cpp
@@ -5,3 +5,99 @@ class_Fr Fr;
 FUNC(void, FR_CODE) class_Fr::InitFunction(void){
 }
 
+FUNC(void, FR_CODE) class_Fr::ControllerInit(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SendMTS(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::StopMTS(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::CheckMTS(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::StartCommunication(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::AllowColdStart(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::HaltCommunication(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::AbortCommunication(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SendWUP(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SetWakeupChannel(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SetExtSync(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetSyncState(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetPOCStatus(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::TransmitTxLPdu(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::ReceiveRxLPdu(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::CheckTxLPduStatus(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::PrepareLPdu(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetGlobalTime(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetNmVector(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SetAbsoluteTimer(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::SetRelativeTimer(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::CancelAbsoluteTimer(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::CancelRelativeTimer(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::EnableAbsoluteTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::EnableRelativeTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::AckAbsoluteTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::AckRelativeTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::DisableAbsoluteTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::DisableRelativeTimerIRQ(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetAbsoluteTimerIRQStatus(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::GetRelativeTimerIRQStatus(void){
+}
+
+FUNC(void, FR_CODE) class_Fr::MainFunction(void){
+}
+
--- LATEST/Fr.h
+++ LATEST/Fr.h
@@ -5,7 +5,39 @@
 
 class class_Fr{
    public:
-      FUNC(void, FR_CODE) InitFunction(void);
+      FUNC(void, FR_CODE) InitFunction              (void);
+      FUNC(void, FR_CODE) ControllerInit            (void);
+      FUNC(void, FR_CODE) SendMTS                   (void);
+      FUNC(void, FR_CODE) StopMTS                   (void);
+      FUNC(void, FR_CODE) CheckMTS                  (void);
+      FUNC(void, FR_CODE) StartCommunication        (void);
+      FUNC(void, FR_CODE) AllowColdStart            (void);
+      FUNC(void, FR_CODE) HaltCommunication         (void);
+      FUNC(void, FR_CODE) AbortCommunication        (void);
+      FUNC(void, FR_CODE) SendWUP                   (void);
+      FUNC(void, FR_CODE) SetWakeupChannel          (void);
+      FUNC(void, FR_CODE) SetExtSync                (void);
+      FUNC(void, FR_CODE) GetSyncState              (void);
+      FUNC(void, FR_CODE) GetPOCStatus              (void);
+      FUNC(void, FR_CODE) TransmitTxLPdu            (void);
+      FUNC(void, FR_CODE) ReceiveRxLPdu             (void);
+      FUNC(void, FR_CODE) CheckTxLPduStatus         (void);
+      FUNC(void, FR_CODE) PrepareLPdu               (void);
+      FUNC(void, FR_CODE) GetGlobalTime             (void);
+      FUNC(void, FR_CODE) GetNmVector               (void);
+      FUNC(void, FR_CODE) SetAbsoluteTimer          (void);
+      FUNC(void, FR_CODE) SetRelativeTimer          (void);
+      FUNC(void, FR_CODE) CancelAbsoluteTimer       (void);
+      FUNC(void, FR_CODE) CancelRelativeTimer       (void);
+      FUNC(void, FR_CODE) EnableAbsoluteTimerIRQ    (void);
+      FUNC(void, FR_CODE) EnableRelativeTimerIRQ    (void);
+      FUNC(void, FR_CODE) AckAbsoluteTimerIRQ       (void);
+      FUNC(void, FR_CODE) AckRelativeTimerIRQ       (void);
+      FUNC(void, FR_CODE) DisableAbsoluteTimerIRQ   (void);
+      FUNC(void, FR_CODE) DisableRelativeTimerIRQ   (void);
+      FUNC(void, FR_CODE) GetAbsoluteTimerIRQStatus (void);
+      FUNC(void, FR_CODE) GetRelativeTimerIRQStatus (void);
+      FUNC(void, FR_CODE) MainFunction              (void);
 };
 
 extern class_Fr Fr;
