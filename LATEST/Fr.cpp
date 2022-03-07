037209fb1b1e2811fcf0ee78b5408ff42b81bc7f
root <nagaraja.hm.0001@gmail.com>
2022-03-07 12:10:07 +0100

    Update: submodule Fr

--- LATEST/Fr.cpp
+++ LATEST/Fr.cpp
@@ -24,8 +24,6 @@
 /*****************************************************/
 class module_Fr:
       public abstract_module
-   ,  public interface_Fr_EcuM
-   ,  public interface_Fr_SchM
 {
    public:
       FUNC(void, FR_CODE) InitFunction   (void);
@@ -44,10 +42,9 @@ class module_Fr:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Fr Fr;
-
-interface_Fr_EcuM *EcuM_Client_ptr_Fr = &Fr;
-interface_Fr_SchM *SchM_Client_ptr_Fr = &Fr;
+module_Fr      Fr;
+infEcuMClient* gptrinfEcuMClient_Fr = &Fr;
+infSchMClient* gptrinfSchMClient_Fr = &Fr;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
