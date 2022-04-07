57c580a83039cdf171c437a77153c3a6142f7b03
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:50:17 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Fr

--- LATEST/Fr.cpp
+++ LATEST/Fr.cpp
@@ -96,8 +96,14 @@ FUNC(void, FR_CODE) module_Fr::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_Fr as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == Fr_InitCheck)
