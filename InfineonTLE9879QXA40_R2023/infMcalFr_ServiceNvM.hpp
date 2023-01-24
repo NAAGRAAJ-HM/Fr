ba9d3621629dde6c07f6307ab6235d76d3325aec
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:39 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for flexray communication protocol stack

---
+++ InfineonTLE9879QXA40_R2023/infMcalFr_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalFr_ServiceNvM.hpp                                          */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALFR_AR_RELEASE_VERSION_MAJOR                                   4
+#define CONSTMCALFR_AR_RELEASE_VERSION_MINOR                                   3
+
+#define Const_DefaultInit_ConstMcalFr                                                                          \
+   ,  {CONSTMCALFR_AR_RELEASE_VERSION_MAJOR, CONSTMCALFR_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
